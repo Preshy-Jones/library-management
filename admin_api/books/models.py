a3871888dbb87cf2ef18e86479180af67f6eaba4
Precious Adedibu <adedibuprecious@gmail.com>
2025-03-01 16:11:13 +0100

    added models and migrations

--- admin_api/books/models.py
+++ admin_api/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
--- frontend_api/books/models.py
+++ frontend_api/books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
+    due_date = models.DateField(null=True, blank=True)
+
+class Borrow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateField(auto_now_add=True)
+    due_days = models.PositiveIntegerField()
+    due_date = models.DateField()
